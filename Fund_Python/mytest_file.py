a1735acf16ad0546e05d4746570ffb168f846d34
Protixer <gambler4o0o0@gmail.com>
2024-01-30 21:30:19 +0200

    Testing List Comprehension

--- Fund_Python/mytest_file.py
+++ Fund_Python/mytest_file.py
@@ -1,7 +1,17 @@
 """List Swap"""
 
-list = ["one", "two", "three", "four", "five", "six"]
+# list = ["one", "two", "three", "four", "five", "six"]
 
-list[3],list[5],list[1] = list[5],list[3], list[0]
+# list[3],list[5],list[1] = list[5],list[3], list[0]
 
-print(list)
+# print(list)
+
+""" List Comprehension """
+quote = "life, uh, finds a way"
+print({char for char in quote if char in "aeiou"})
+
+print([(x, y) for x in [1,2,3] for y in [3,1,4]])
+
+print([x for x in range(0, 10)])
+
+print([ char for char in quote])
