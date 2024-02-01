5178ffb882f0d96059eb8f91d832764411df4273
Protixer <gambler4o0o0@gmail.com>
2024-02-01 22:59:35 +0200

    Second option

--- Fund_Python/Lists Exercise/survival_of_the_biggest.py
+++ Fund_Python/Lists Exercise/survival_of_the_biggest.py
@@ -1,14 +1,31 @@
+""" First option"""
+# text = input().split(' ')
+# text = [int(i) for i in text]
+
+# n = int(input())
+# deleted_items = []
+
+# for i in range(0,n):
+#     min_number = min(text)
+#     deleted_items.append(min_number)
+#     text.remove(min_number)
+
+# text = ", ".join(str(el) for el in text)
+# print(text)
+
+""" Second option"""
 text = input().split(' ')
 text = [int(i) for i in text]
 
 n = int(input())
-deleted_items = []
 
 for i in range(0,n):
-    min_number = min(text)
-    deleted_items.append(min_number)
-    text.remove(min_number)
+    min_value = text[0]
+    for j in range(0, len(text)):
+        if min_value > text[j]:
+            min_value = text[j]
 
-print(type(text))
+    text.remove(min_value)
+
 text = ", ".join(str(el) for el in text)
 print(text)
