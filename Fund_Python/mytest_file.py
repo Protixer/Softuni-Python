c2130e3f79188896d30a28942d149fad361621fc
Protixer <gambler4o0o0@gmail.com>
2024-02-01 23:21:11 +0200

    Testing all build in func on lists

--- Fund_Python/mytest_file.py
+++ Fund_Python/mytest_file.py
@@ -1,17 +1,21 @@
 """List Swap"""
-
 # list = ["one", "two", "three", "four", "five", "six"]
-
 # list[3],list[5],list[1] = list[5],list[3], list[0]
-
 # print(list)
 
 """ List Comprehension """
 # quote = "life, uh, finds a way"
 # print({char for char in quote if char in "aeiou"})
-
 # print([(x, y) for x in [1,2,3] for y in [3,1,4]])
-
 # print([x for x in range(0, 10)])
-
 # print([char for char in quote])
+
+""" List Append"""
+# fruits = ["apple","banana","blueberry"]
+# fruits.append("nar")
+# print(fruits)
+
+""" List Clear """
+fruits = ["apple","banana","blueberry"]
+fruits.clear()
+print(fruits)
