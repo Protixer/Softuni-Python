2ac59744c7a986081ed82e97123d74d73b3d47c3
Protixer <gambler4o0o0@gmail.com>
2024-02-05 23:30:37 +0200

    Solve Exercise Calculation

--- Fund_Python/Functions/calculation.py
+++ Fund_Python/Functions/calculation.py
@@ -3,7 +3,7 @@ def multiply(num_one, num_two):
 
 
 def divide(num_one, num_two):
-    return num_one / num_two
+    return int(num_one / num_two)
 
 
 def add(num_one, num_two):
@@ -17,3 +17,12 @@ def subtract(num_one, num_two):
 command = input()
 number_one = int(input())
 number_two = int(input())
+
+if command == "multiply":
+    print(multiply(number_one, number_two))
+elif command == "divide":
+    print(divide(number_one, number_two))
+elif command == "add":
+    print(add(number_one, number_two))
+elif subtract == "subtract":
+    print(subtract(number_one, number_two))
