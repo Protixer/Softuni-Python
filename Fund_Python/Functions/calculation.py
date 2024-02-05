e75960ffe5374fdbb97724393ef2f9aa25d0e795
Protixer <gambler4o0o0@gmail.com>
2024-02-05 23:25:07 +0200

    add functions

---
+++ Fund_Python/Functions/calculation.py
@@ -0,0 +1,19 @@
+def multiply(num_one, num_two):
+    return num_one * num_two
+
+
+def divide(num_one, num_two):
+    return num_one / num_two
+
+
+def add(num_one, num_two):
+    return num_one + num_two
+
+
+def subtract(num_one, num_two):
+    return num_one - num_two
+
+
+command = input()
+number_one = int(input())
+number_two = int(input())
