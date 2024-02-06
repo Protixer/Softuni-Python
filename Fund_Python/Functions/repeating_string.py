280d3521c4d15f6b1e14d18e47722d0be58cf56c
Protixer <gambler4o0o0@gmail.com>
2024-02-06 23:42:34 +0200

    String repeat

--- Fund_Python/Functions/repeating_string.py
+++ Fund_Python/Functions/repeating_string.py
@@ -1,8 +1,14 @@
 """ Repeating String with lambda"""
 
+# string = input()
+# n = int(input())
+
+# repeat_string = lambda a, b: a * b
+# string = repeat_string(string, n)
+# print(string)
+
+""" Repeating string with multiply"""
+
 string = input()
 n = int(input())
-
-repeat_string = lambda a, b: a * b
-string = repeat_string(string, n)
-print(string)
+print(string * n)
