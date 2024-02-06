575b97da45ae1691a930da0a01957a27cb84f1a8
Protixer <gambler4o0o0@gmail.com>
2024-02-06 23:53:39 +0200

    Solve orders

---
+++ Fund_Python/Functions/orders.py
@@ -0,0 +1,19 @@
+def total_price(product, quantity):
+    final_price = 0
+
+    if product == "coffee":
+        final_price = quantity * 1.50
+    elif product == "water":
+        final_price = quantity * 1
+    elif product == "coke":
+        final_price = quantity * 1.4
+    elif final_price == "snacks":
+        final_price = quantity * 2
+
+    return final_price
+
+
+product = input()
+quantity = int(input())
+final_price = total_price(product, quantity)
+print(f"{final_price:.2f}")
