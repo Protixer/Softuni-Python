116192bc22333da046258144868eec4956de569a
Protixer <gambler4o0o0@gmail.com>
2024-02-07 22:39:17 +0200

    my_test_file.py

---
+++ test.py
@@ -0,0 +1,16 @@
+# cars = {
+#     **{f"http://saffsafsafas.coom/page={p}": "General" for p in range(10)},
+#     **{f"http://saffsafsafas.coom/page={p}": "Guidance" for p in range(10)},
+# }
+# for url, annotation in cars.items():
+#     print(f"{url} => {annotation}")
+
+# print(
+#     f"Simona e  asfjsafhjosuafgosaf ojafsija iofsaj iopsajf iposajfopi jsafposafdsafsafsafsafsafsafsafsafsafsafsaffjsfapjsafpjsfapjiosafjpsafjpoiasfjposafjopojpsafojpsafjopsafjopasojpsaojpfsajopasjofkasojpfasojpfsaojpsfaprudla"
+# )
+
+
+# """ NameError: name 'mesage' is not defined """
+# message = "Hello Python Crash Course reader!"
+# print(mesage)
+
