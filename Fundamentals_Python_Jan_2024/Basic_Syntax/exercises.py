014cb86340d9f0af69b7912afc6c6a3614096778
protixer <gambler4o0o0@gmail.com>
2026-05-20 21:18:15 +0300

    Learning Python

--- Fundamentals_Python_Jan_2024/Basic_Syntax/exercises.py
+++ Fundamentals_Python_Jan_2024/Basic_Syntax/exercises.py
@@ -1,5 +1,18 @@
 # print("Hello World...")
 
-a = 5
-result = '"a" is greater than 30' if a > 20 else '"a" is less than 20'
-print(result)
+# a = 5
+# result = '"a" is greater than 30' if a > 20 else '"a" is less than 20'
+# print(result)
+
+
+# FOR LOOP
+# word = input()
+# reversed_word = ""
+# for i in range(len(word) -1 , -1, -1):
+#     reversed_word += word[i]
+# print(reversed_word)
+
+# reversed word
+# word = input()
+# reversed_word = word[::-1]
+# print(reversed_word)
